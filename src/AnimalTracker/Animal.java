6d388e1d2a4fe9eedb2d19696b093716f2787517
Mutsuki Imai <mimai@catalyte.io>
2024-05-11 22:32:58 -0400

    Added additional implementations & records

--- src/AnimalTracker/Animal.java
+++ src/AnimalTracker/Animal.java
@@ -1,9 +1,78 @@
 package AnimalTracker;
 
-interface FlightEnabled {
+enum FlightStages implements Trackable {GROUNDED, LAUNCH, CRUISE, DATA_COLLECTION;
+
+    @Override
+    public void track() {
+        if (this != GROUNDED) {
+            System.out.println("Monitoring " + this);
+        }
+    }
+};
+record DragonFly(String name, String type) implements FlightEnabled {
+
+    @Override
+    public void takeOff() {
+
+    }
+
+    @Override
+    public void land() {
+
+    }
+
+    @Override
+    public void fly() {
+
+    }
+}
+class Satellite implements OrbitEarth {
+
+    public void achieveOrbit(){
+        System.out.println("Orbit achieved");
+    }
+
+    @Override
+    public void takeOff() {
+
+    }
+
+    @Override
+    public void land() {
+
+    }
+
+    @Override
+    public void fly() {
+
+    }
+}
+
+interface OrbitEarth extends FlightEnabled {
+
+    void achieveOrbit();
 
 }
 
-interface Trackable {}
+interface FlightEnabled {
+
+    //
+    double MILES_TO_KM = 1.60934;
+
+    double KM_TO_MILES = 0.621371;
+
+//    public abstract void takeOff();
+    void takeOff();
+
+//    abstract void land();
+    void land();
+
+    void fly();
+}
+
+interface Trackable {
+    void track();
+}
 public abstract class Animal {
+    public abstract void move();
 }
--- src/AnimalTracker/Main.java
+++ src/AnimalTracker/Main.java
@@ -2,6 +2,33 @@ package AnimalTracker;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Bird bird = new Bird();
+        Animal animal = bird; // Assigned `bird` to an `Animal` reference
+        FlightEnabled flier = bird; // " to a `FlightEnabled` reference
+        Trackable tracked = bird; // " to a `Trackable` reference
+
+        animal.move();
+        //  flier.move();
+        //  tracked.move();
+//        flier.takeOff();
+//        flier.fly();
+//        tracked.track();
+//        flier.land();
+
+    }
+
+    // `inFlight` method will take a `FlightEnabled` `bird` reference
+    private static void inFlight (FlightEnabled flier) {
+        flier.takeOff(); // Method specific to `flier`
+        flier.fly(); // "
+
+        // Checks if this `FlightEnabled` `bird` reference is
+        // also an instance of `Trackable`
+        if (flier instanceof Trackable tracked) {
+            tracked.track(); // If yes, track `bird`
+        }
+
+        flier.land();
     }
 }
